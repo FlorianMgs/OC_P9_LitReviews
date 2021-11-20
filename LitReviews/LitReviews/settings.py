99c8f61149ec6266c2d6142b01d7f5bc9ea34b9d
FlorianMgs <madgic.shrooms@gmail.com>
2021-11-20 17:13:02 +0100

    few fixes

--- LitReviews/LitReviews/settings.py
+++ LitReviews/LitReviews/settings.py
@@ -134,7 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authentication.User'
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'feed'
+LOGIN_REDIRECT_URL = 'discover'
 LOGOUT_REDIRECT_URL = LOGIN_URL
 
 MEDIA_URL = '/media/'
